9d59e805f6b2c48b244cf9775937b22f6e202917
Loris Marino <loris.mro@gmail.com>
2022-01-07 20:45:35 +0100

    chore(stylelint): add scss syntax

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,6 +2,7 @@ module.exports = {
   extends: [
     'stylelint-config-standard',
     'stylelint-config-prettier',
+    'stylelint-config-standard-scss',
     'stylelint-config-sass-guidelines',
   ],
   plugins: ['stylelint-order'],
